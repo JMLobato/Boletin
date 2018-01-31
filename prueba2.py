20e2541aeb0132b9b8920afbf03e1c6d6b3e4583
JMLobato <derc8aid@gmail.com>
2018-01-31 13:26:24 +0100

    He modificado los ejercicios

--- prueba2.py
+++ prueba2.py
@@ -1,7 +1,11 @@
 from boletin8 import convertir_a_euros
 with open("comunicaciones.txt","r") as archivo:
-	contenido= archivo.readlines().strip("\n")
+	contenido= archivo.readlines()
+lista=[]
 for linea in contenido:
 	if linea != "Tarifa 3":
-		cosasdelinea=linea.split(":")
-print(cosasdelinea)
+		tiempos=linea.strip("\n").split(":")
+		lista.append(tiempos)
+for tiempo in lista:
+	comunicacion=convertir_a_euros(tiempo[0],tiempo[1],tiempo[2],3)
+	print("Esta comunicacion ha costado %d euros y %d centimos"%(euros,cent))
