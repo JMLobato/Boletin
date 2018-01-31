b6a6cf99e51618ab409ca1c149b2a7fdabd91e4b
JMLobato <derc8aid@gmail.com>
2018-01-31 11:59:27 +0100

    He modificado el archivo con funciones

--- boletin8.py
+++ boletin8.py
@@ -3,8 +3,9 @@ def pasar_a_segundos(h,m,s):
 	return total_seg
 
 def calcular_coste(h,m,s):
-	total=pasar_a_segundos(h,m,s)*100 #si 1 euro son X segundos, el calculo en cent se realizará multiplicando esos segundos por 100
+	total=(pasar_a_segundos(h,m,s))*100 #si 1 euro son X segundos, el calculo en cent se realizará multiplicando esos segundos por 100
 	return total
 
 def convertir_a_euros(h,m,s):
-
+cent=calcular_coste(h,m,s)
+total=(cent/100)%100
