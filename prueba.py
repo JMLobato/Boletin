aae83a8ef2c278ab6d9f649f4c2746aaf4613949
JMLobato <derc8aid@gmail.com>
2018-01-31 13:07:05 +0100

    He modificado el segundo apartado de los ejercicios

--- prueba.py
+++ prueba.py
@@ -1,4 +1,4 @@
-from boletin8 import convertir_a_euros,calcular_coste,pasar_a_segundos
+from boletin8 import convertir_a_euros
 tarifa=float(input("¿Cual es tu tarifa en centimos por segundo? "))
 com=int(input("¿Cuantas comunicaciones se han realizado? "))
 cont=0
@@ -7,5 +7,5 @@ while cont != com:
 	minutos=int(input("¿Cuantas minutos duró? "))
 	horas=int(input("¿Cuantas horas duró? "))
 	euros,cent=convertir_a_euros(horas,minutos,segundos,tarifa)
-	print("La comunicación nº%d costó %d, %d"%((cont+1),euros,cent))
+	print("La comunicación nº%d costó %d euros y %d centimos"%((cont+1),euros,cent))
 	cont+=1
--- prueba2.py
+++ prueba2.py
@@ -1,1 +1,7 @@
-prueba2.py
+from boletin8 import convertir_a_euros
+with open("comunicaciones.txt","r") as archivo:
+	contenido= archivo.readlines().strip("\n")
+for linea in contenido:
+	if linea != "Tarifa 3":
+		cosasdelinea=linea.split(":")
+print(cosasdelinea)
